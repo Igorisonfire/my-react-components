ea7b456ae42c50ee7f67feac32dc222b71397fe7
igorburak <biv992@gmail.com>
2020-08-13 01:29:45 +0200

    check  gh-pages

---
+++ src/stories/3-Button.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@storybook/react/demo';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+export const Text = () => <Button onClick={action('clicked')}>333333</Button>;
+
+export const Emoji = () => (
+  <Button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool">
+        333333333333333
+    </span>
+  </Button>
+);

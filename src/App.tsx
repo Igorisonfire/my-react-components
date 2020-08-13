c54ddfc7eaea526e702ed01d4c01a2c876c2d31b
igorburak <biv992@gmail.com>
2020-08-13 04:32:22 +0200

    add gh-pages action

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React111333
+          Learn React
         </a>
       </header>
     </div>
